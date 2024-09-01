5fc67e12fe17df44c667452d3a94b5ab2ca0df7f
Venkatesh Pillay <venkateshpillay@Venkateshs-Laptop.local>
2024-09-01 15:05:46 +0530

    changes text

--- app/src/main/java/com/scanners/docsscanners/screens/common/ErrorScreen.kt
+++ app/src/main/java/com/scanners/docsscanners/screens/common/ErrorScreen.kt
@@ -2,18 +2,36 @@ package com.scanners.docsscanners.screens.common
 
 import androidx.compose.runtime.Composable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.scanners.docsscanners.R
+
 
 @Composable
 fun ErrorScreen(message: String) {
     Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(10.dp, 0.dp),
+        contentAlignment = Alignment.Center,
     ){
-        Text(text = message, fontSize = 32.sp)
+        Column (
+           horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = message, fontSize = 32.sp)
+            Spacer(modifier = Modifier.width(10.dp))
+            Text(text = stringResource(id = R.string.errorDescription), fontSize = 16.sp, textAlign = TextAlign.Center)
+        }
     }
 }
--- app/src/main/java/com/scanners/docsscanners/screens/home/HomeScreen.kt
+++ app/src/main/java/com/scanners/docsscanners/screens/home/HomeScreen.kt
@@ -25,6 +25,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
